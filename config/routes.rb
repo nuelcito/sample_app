7a66998b663d2167ae5335ee9cb2f8707f21240b
Nuel <manuel.gonz.is@gmail.com>
2010-03-30 16:38:51 -0500

    Removed default Rails page

--- config/routes.rb
+++ config/routes.rb
@@ -43,4 +43,5 @@ ActionController::Routing::Routes.draw do |map|
   map.contact '/contact', :controller => 'pages', :action => 'contact'
   map.about '/about', :controller => 'pages', :action => 'about'
   map.help '/help', :controller => 'pages', :action => 'help'
+  map.root :controller => 'pages', :action => 'home'
 end
