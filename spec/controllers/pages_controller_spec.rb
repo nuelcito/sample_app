5d25557eecd3c2367974261fb7fc5b545f6dfc8f
Nuel <manuel.gonz.is@gmail.com>
2010-03-30 12:18:39 -0500

    Help Page Addition

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -4,6 +4,10 @@ describe PagesController do
 
   integrate_views
 
+  before(:each) do
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
   #Delete these examples and add some real ones
   it "should use PagesController" do
     controller.should be_an_instance_of(PagesController)
@@ -18,7 +22,7 @@ describe PagesController do
 
     it "should have the right title" do
       get 'home'
-      response.should have_tag("title", "Ruby on Rails Tutorial Sample App | Home")
+      response.should have_tag("title", @base_title + " | Home")
     end
   end
 
@@ -31,7 +35,7 @@ describe PagesController do
     
     it "should have the right title" do
       get 'contact'
-      response.should have_tag("title", "Ruby on Rails Tutorial Sample App | Contact")
+      response.should have_tag("title", @base_title + " | Contact")
     end
   end
 
@@ -43,7 +47,21 @@ describe PagesController do
     
     it "should have the right title" do
       get 'about'
-      response.should have_tag("title", "Ruby on Rails Tutorial Sample App | About")
+      response.should have_tag("title", @base_title + " | About")
     end
   end
+
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'help'
+      response.should have_tag("title", @base_title + " | Help")
+    end
+
+  end
+  
 end
